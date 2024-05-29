c5916ac085a4b33fed3f4702e5feef15f7cb00d3
natorin54 <n_atorin@mail.ru>
2024-05-29 21:48:58 +0700

    homework1.py

--- homework1.py
+++ homework1.py
@@ -1,7 +1,6 @@
 example = str(input())
-middle = len(example) // 2
 print("Первый символ: " + example[0])
 print("Последний символ: " + example[-1])
-print("Вторая половина строки: " + example[middle:])
+print("Вторая половина строки: " + example[len(example) // 2:])
 print("Слово наоборот: " + example[::-1])
 print("Каждый второй символ строки: " + example[1::2])
