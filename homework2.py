1005819615d9cdb054c0fc1b9d7df5719e8b30a3
natorin54 <n_atorin@mail.ru>
2024-05-30 20:36:55 +0700

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+homework_sum = 12
+hours_sum = 1.5
+course_name = 'Python'
+time_per_hw = hours_sum / homework_sum
+print('Курс:', course_name, ', всего задач:', homework_sum, ', затрачено часов:', hours_sum, ', среднее время выполнения:', time_per_hw, 'часа')
