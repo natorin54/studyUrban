837314990f0e75cf4f97557f386726c94dcf9a0f
natorin54 <n_atorin@mail.ru>
2024-06-02 17:36:01 +0700

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+my_string = input("Введите строку: ")
+print("Количество символов в строке: ", len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print("Первый символ строки: ", my_string[0])
+print("Последний символ строки: ", my_string[-1])
