b7991601e0e45341d0c0fb7c7445b297895cfb25
natorin54 <n_atorin@mail.ru>
2024-06-03 21:25:49 +0700

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,8 @@
+grades = [[5,3,3,5,4], [2,2,2,3], [4,5,5,2], [4,4,3], [5,5,5,4,5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students_list = list(students)
+students_list.sort()
+journal = {}
+for i in range(len(students_list)):
+    journal[students_list[i]] = sum(grades[i]) / len(grades[i])
+print(journal)
